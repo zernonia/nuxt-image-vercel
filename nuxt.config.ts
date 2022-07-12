02d3c4d7f31a2d7367c1fdeb735ac5e77b3a23ed
zernonia <zernonia@gmail.com>
2022-07-13 00:38:48 +0800

    1 binaries uploaded

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,9 @@
-import { defineNuxtConfig } from 'nuxt'
+import { defineNuxtConfig } from 'nuxt';
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-
-})
+  modules: ['@nuxt/image-edge'],
+  image: {
+    domains: ['avatars0.githubusercontent.com', 'images.unsplash.com'],
+  },
+});
